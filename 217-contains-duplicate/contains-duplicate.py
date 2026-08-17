ee5393ce97be83c08cc3c70ca1efb725024c336e
Sriharsha2301 <nethisriharsha@gmail.com>
2026-08-17 17:55:07 +0530

    Time: 85 ms (8.81%) | Memory: 21 MB (93.76%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -6,11 +6,12 @@ class Solution(object):
         """
         n=len(nums)
         nums1=sorted(nums)
-        i=0
         for i in range(n-1):
             if nums1[i]==nums1[i+1]:
                 return True
         return False
+
+
 
 
 
