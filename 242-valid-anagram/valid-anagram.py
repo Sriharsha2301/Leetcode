53069b7d704b5e47344a810f62ed01ed70e6a015
Sriharsha2301 <nethisriharsha@gmail.com>
2026-08-18 16:43:37 +0530

    Time: 23 ms (49.52%) | Memory: 13.4 MB (40.44%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        n1=sorted(s)
+        n2=sorted(t)
+        if n1==n2:
+            return True
+        else:
+            return False
+
+        
+        
