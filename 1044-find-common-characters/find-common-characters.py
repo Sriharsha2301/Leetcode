5bc075f4e05a2faee651c1c63d78d7409a4895f1
Sriharsha2301 <nethisriharsha@gmail.com>
2026-08-19 00:11:12 +0530

    Time: 39 ms (6.26%) | Memory: 12.3 MB (72.48%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -5,19 +5,15 @@ class Solution(object):
         :rtype: List[str]
         """
         result=[]
-         # Initialize common_character_counts with the characters from the first word
-        common_char_count=Counter(words[0])
-         # Count characters in the current word
+        common_character_count=Counter(words[0])
 
         for i in range(1,len(words)):
-            current_char_count=Counter(words[i])
+            current_character_count=Counter(words[i])
 
-        # Update the common character counts to keep the minimum counts
-
-            for letter in common_char_count.keys():
-                common_char_count[letter]=min(common_char_count[letter],current_char_count[letter]
-                )
-        for letter,count in common_char_count.items():
+            for letter in common_character_count.keys():
+                common_character_count[letter]=min(common_character_count[letter],current_character_count[letter])
+        
+        for letter,count in common_character_count.items():
             for _ in range(count):
                 result.append(letter)
         return result
