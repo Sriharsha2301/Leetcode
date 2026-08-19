98a4850a497519dd9ae57e6d835d77ca63f31442
Sriharsha2301 <nethisriharsha@gmail.com>
2026-08-19 14:10:42 +0530

    Time: 2 ms (34.49%) | Memory: 12.5 MB (19.05%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        l=[]
+        r=[]
+        for i in s:
+            if  i!="#":
+                l.append(i)
+            else:
+                if l:
+                    l.pop()
+        for j in t:
+            if  j!="#":
+                r.append(j)
+            else:
+                if r:
+                    r.pop()
+        return ("".join(l)=="".join(r))
+        
+
+        
