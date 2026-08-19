04ee47faa84441aca62c14b32db40f49379db734
Sriharsha2301 <nethisriharsha@gmail.com>
2026-08-19 22:03:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (71.95%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l=[]
+        for i in s:
+                if i=="(" or i=="{" or i=="[":
+                    l.append(i)
+                else:
+                    if l:
+                        if i==")" and l[-1]=="(":
+                            l.pop()
+                        elif i=="]" and l[-1]=="[":
+                            l.pop()
+                        elif i=="}" and l[-1]=="{":
+                            l.pop()
+                        else:
+                            return False
+                    else:
+                        return False
+        if  not l:
+            return True
+        else:
+            return False
+                        
+        
+        
+        
+            
+        
